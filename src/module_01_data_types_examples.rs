a9cbe50acc175d045015a20b93e69c1846bb2f7a
Mrinal Singh <mrinalsingh7000@gmail.com>
2026-01-10 00:31:48 +0530

    Add arithmetic operations

--- src/module_01_data_types_examples.rs
+++ src/module_01_data_types_examples.rs
@@ -1,4 +1,4 @@
-fn primitive_data_types() {
+pub fn primitive_data_types() {
     // Integer types
     let token_supply: u128 = 1_000_000_000_000;
     let block_number: i64 = -1234567890;
@@ -27,7 +27,41 @@ fn primitive_data_types() {
     println!("Contract Name (String): {}", contract_name);
 }
 
+pub fn arithmetic_operations() {
+    let account_balance: i32 = 1000;
+    let transaction_amount: i32 = 250;
+
+    println!(
+        "Account Balance:{}, Transaction Amount:{}",
+        account_balance, transaction_amount
+    );
+    println!(
+        "New Balance after transaction:{}",
+        account_balance - transaction_amount
+    ); // Subtraction
+    println!(
+        "Double transaction amount (for staking):{}",
+        transaction_amount * 2
+    ); // Multiplication for staking rewards
+    println!(
+        "Division of shared distribution: 1000 /4 = {}",
+        account_balance / 4
+    ); // example of distributing funds between 4 parties
+    println!(
+        "Reaminder when dividing transaction fee: 1000 % 3={}",
+        account_balance % 3
+    ); // Modulus for fee calculation
+
+    let gas_price: f64 = 0.00000002;
+    let gas_used: f64 = 21000.0;
+    println!("Gas Price = {}, Gas Used = {}", gas_price, gas_used);
+    println!("Total Gas Fee: {:.8}", gas_price * gas_used);// :.8 for precision 
+}
+
 pub fn demo() {
     println!("\n");
     primitive_data_types();
+
+    println!("\n");
+    arithmetic_operations();
 }
