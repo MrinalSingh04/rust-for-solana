910a621419b211686607218b1d8dea2979b7b85d
Mrinal Singh <mrinalsingh7000@gmail.com>
2026-01-20 23:58:05 +0530

    add module_03_functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // mod module_01_data_types_examples;
-mod module_02_control_flow_examples;
+// mod module_02_control_flow_examples;
+mod module_03_functions_closure_examples;
 fn main() {
     // module_01_data_types_examples::demo();
-    module_02_control_flow_examples::demo();
+    // module_02_control_flow_examples::demo();
+    module_03_functions_closure_examples::demo();
 }
---
+++ src/module_03_functions_closure_examples.rs
@@ -0,0 +1,7 @@
+pub fn basic_function() {
+    fn add_gas_fee(base_fee: i32, gas_used: i32) -> i32 {
+        base_fee + gas_used
+    }
+}
+
+pub fn demo() {}
